7f50dad8e1dae6abaeb4697912a30e86e501193a
Kyle Alderman <k.alderman922@gmail.com>
2022-12-15 16:34:03 -0800

    Add remove grid function that clears the grid

--- script.js
+++ script.js
@@ -1,23 +1,27 @@
 const container = document.querySelector('#container');
 
-
-let gridNum = 16;
-let userSelection = prompt('Enter Grid Size from 0 - 100:');
-gridNum = userSelection;
+let gridNum = prompt('Enter Grid Size from 1 - 100:')
+makeGrid(gridNum);
 
 function makeGrid(gridNum) {
-   let totalDivs = gridNum * gridNum;
-   container.style.gridTemplateColumns = `repeat(${gridNum}, auto)`;
-   container.style.gridTemplateRows = `repeat(${gridNum}, auto)`;
-   for (i = 1; i <= totalDivs; i++) {
+  let totalDivs = gridNum * gridNum;
+  container.style.gridTemplateColumns = `repeat(${gridNum}, auto)`;
+  container.style.gridTemplateRows = `repeat(${gridNum}, auto)`;
+  for (i = 1; i <= totalDivs; i++) {
     const gridElement = document.createElement('div');
     container.appendChild(gridElement);
     gridElement.addEventListener('mouseover', () => {
-        gridElement.classList.add('hovered');
+      gridElement.classList.add('hovered');
     });
    }
+};
+
+function removeGrid() {
+    let hoveredElements = container.querySelectorAll('div');
+    hoveredElements.forEach(hoveredElement => hoveredElement.remove());
+    makeGrid(gridNum);
+};
+
+function changeGrid() {
 }
 
-
-
-makeGrid(gridNum);
