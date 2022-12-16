cc2baf66ae104038749f9574de1ceda885e08a60
Kyle Alderman <k.alderman922@gmail.com>
2022-12-15 17:00:29 -0800

    Add condition is grid num is greater than 100 and add change grid function

--- script.js
+++ script.js
@@ -1,19 +1,24 @@
 const container = document.querySelector('#container');
 
-let gridNum = prompt('Enter Grid Size from 1 - 100:')
-makeGrid(gridNum);
+let gridNum = prompt('Enter Grid Size from 1 - 100:');
 
 function makeGrid(gridNum) {
   let totalDivs = gridNum * gridNum;
-  container.style.gridTemplateColumns = `repeat(${gridNum}, auto)`;
-  container.style.gridTemplateRows = `repeat(${gridNum}, auto)`;
-  for (i = 1; i <= totalDivs; i++) {
-    const gridElement = document.createElement('div');
-    container.appendChild(gridElement);
-    gridElement.addEventListener('mouseover', () => {
-      gridElement.classList.add('hovered');
-    });
-   }
+  if(gridNum > 100) {
+    alert('Enter a number less than 100')
+    location.reload();
+  }
+  else {
+    container.style.gridTemplateColumns = `repeat(${gridNum}, auto)`;
+    container.style.gridTemplateRows = `repeat(${gridNum}, auto)`;
+    for (i = 1; i <= totalDivs; i++) {
+      const gridElement = document.createElement('div');
+      container.appendChild(gridElement);
+      gridElement.addEventListener('mouseover', () => {
+        gridElement.classList.add('hovered');
+      });
+     }
+  }
 };
 
 function removeGrid() {
@@ -23,5 +28,7 @@ function removeGrid() {
 };
 
 function changeGrid() {
+    location.reload()
 }
 
+makeGrid(gridNum);
